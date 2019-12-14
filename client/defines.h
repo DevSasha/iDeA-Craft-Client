8a7702d14d0e01f9fe9fb1ea0755bb836d90b9b1
Dr. Sasha <dr.sasha@idea-craft.space>
2019-12-14 18:24:39 +0500

    [UPD] Updated api url

--- client/defines.h
+++ client/defines.h
@@ -2,6 +2,6 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
-#define API_SERVER "http://launcher.idea-craft.space/versions.json"
+#define API_SERVER "https://api.launcher.idea-craft.space/"
 
 #endif // DEFINES_H
