7f321b837e21a6e6148e3cb728343a17966ef450
Dr. Sasha <dr.sasha@idea-craft.space>
2020-01-21 09:18:59 +0500

    [UPD] Update Core::load() Remove old code

--- client/core.cpp
+++ client/core.cpp
@@ -20,23 +20,12 @@ void Core::authorized() {
     window_main->show();
 }
 
-void Core::load()
-{
+void Core::load() {
     qDebug() << "loading...";
-    //cfgs = new QSettings(QStandardPaths::writableLocation(QStandardPaths::AppDataLocation) + "/configs.ini", QSettings::IniFormat);
-    cfgs = new QSettings("DrSasha", "iDeA-Craft Launcher");
-    inst = new Instanse("lite12");
+	window_auth = new Authorization();
         connect(window_auth, &Authorization::authorized, this, &Core::authorized);
-        //connect(window_main, &MainWindow::startInst, this, &Core::startInst);
     Local = new QDir(QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation));
         if(!Local->exists())Local->mkdir(Local->path());
 
-    cfgs->beginGroup("AuthorizationData");
-    if(!cfgs->value("login").isNull() && !cfgs->value("password").isNull()){
-        window_auth->login = cfgs->value("login").toString();
-        window_auth->password = cfgs->value("password").toString();
-        window_auth->auth();
-    }else window_auth->show();
-    cfgs->endGroup();
-    //connect(inst, &Instanse::downloadComplete, this, &Core::downloadComplete);
+	window_auth->show();
 }
